128991422ede58daaf4538549841a87dd3645120
Leithon English <leithonenglish@gmail.com>
2021-11-16 09:49:12 -0500

    added sanity

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+require(`dotenv`).config()
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -29,6 +30,14 @@ module.exports = {
         // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+        token: process.env.SANITY_TOKEN,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
